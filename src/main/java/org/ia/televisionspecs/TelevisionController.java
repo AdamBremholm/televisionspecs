645147fdd9e16c303c7204d3f3de37381af6b2e2
AdamBremholm <adambremholm@gmail.com>
2019-03-01 19:19:38 +0100

    Update getAccesToken and get reviews

--- src/main/java/org/ia/televisionspecs/TelevisionController.java
+++ src/main/java/org/ia/televisionspecs/TelevisionController.java
@@ -1,16 +1,23 @@
 package org.ia.televisionspecs;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.StringTokenizer;
 
 @RestController
 public class TelevisionController {
 
     Storage storage;
     RestTemplate restTemplate;
+
 
     public TelevisionController(Storage storage, RestTemplate restTemplate) {
 
@@ -48,11 +55,48 @@ public class TelevisionController {
             return storage.save(storedTelevision);
         }).orElseThrow( () -> new TelevisionException("No tv with id " + id));
     }
-    @GetMapping("/reviews")
-    public Review getReviews() {
-        Review review = restTemplate.getForObject("http://192.168.1.166:65444/reviews/1", Review.class);
-        System.out.println(review.toString());
-        return review;
+    @GetMapping("/getreviews")
+    public List<Review> getReviews() {
+        String accesstoken = "";
+
+        accesstoken = getAccessToken("user", "password");
+
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(accesstoken);
+        HttpEntity<?> httpEntity = new HttpEntity<Object>(headers);
+        ResponseEntity<List<Review>> responseEntity = restTemplate.exchange("http://localhost:8080/reviews/reviews", HttpMethod.GET, httpEntity, new ParameterizedTypeReference<List<Review>>() {
+        });
+
+        System.out.println(responseEntity.getBody().toString());
+        return responseEntity.getBody();
+    }
+
+    public String getAccessToken(String username, String password) {
+
+        String accessToken ="";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+        HttpEntity<String> entity = new HttpEntity<String>("{\"username\": \""+username+"\",\"password\": \""+password+"\"}", headers);
+        ResponseEntity<String> response = restTemplate.exchange("http://localhost:8080/auth", HttpMethod.POST, entity, String.class);
+        System.out.println("Response: " + response.toString());
+        accessToken = response.getHeaders().get("Authorization").toString();
+        System.out.println(accessToken);
+
+        String[] parts = accessToken.split(" ");
+
+        accessToken = removeLastCharacter(parts[1]); //removes last ] from string
+
+        System.out.println("Final access token: " + accessToken);
+        return accessToken;
+
+    }
+    public String removeLastCharacter(String str) {
+        if (str != null && str.length() > 0 && str.charAt(str.length() - 1) == ']') {
+            str = str.substring(0, str.length() - 1);
+        }
+        return str;
     }
 
 }
